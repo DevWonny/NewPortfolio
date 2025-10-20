80ea90c90e7da86e814ebfe9cb6ee765d5e40904
DevWonny <cjfdnjs1994@naver.com>
2025-10-20 10:51:14 +0900

    각 영역 파일 생성

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,22 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+// component
+import Intro from '@/components/Intro.vue'
+import Information from '@/components/information.vue'
+import Skills from '@/components/Skills.vue'
+import Career from '@/components/Career.vue'
+import Project from '@/components/Project.vue'
+import Contact from '@/components/Contact.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <Intro></Intro>
+    <Information></Information>
+    <Skills></Skills>
+    <Career></Career>
+    <Project></Project>
+    <Contact></Contact>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style scoped></style>
---
+++ src/components/Skills.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>Skills</div>
+</template>
+
+<style scoped></style>
